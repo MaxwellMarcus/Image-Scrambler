bd5320339b9741cd116a67f9e125f31e23273ed4
bigmaxattack37 <bigmaxmarcus@gmail.com>
2019-10-20 11:12:48 -0500

    It's working. I only need to add comments to make it understandable.

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from PIL import Image,ImageTk
+from random import shuffle
+import time
 
 root = Tk()
 
@@ -14,20 +16,30 @@ def get_chunks(picture, num_chunks_x,num_chunks_y):
     chunk_y = int(picture_y/num_chunks_y)
 
     final_picture = []
+    positions = []
 
     for x in range(num_chunks_x):
-        row_chunks = []
         for y in range(num_chunks_y):
             x_pos = x*chunk_x
             y_pos = y*chunk_y
 
             chunk = picture.crop((x_pos,y_pos,x_pos+chunk_x,y_pos+chunk_y))
+            data = chunk.load()
 
-            row_chunks.append(chunk)
+            useful = False
+            for x1 in range(chunk_x):
+                for y1 in range(chunk_y):
+                    if data[x1,y1][0] == 0 and data[x1,y1][1] == 0 and data[x1,y1][2] == 0:
+                        useful = True
 
-        final_picture.append(row_chunks)
+            if useful:
+                final_picture.append(chunk)
+                positions.append((x_pos,y_pos))
 
-    return final_picture
+    shuffle(positions)
+    shuffle(positions)
+
+    return (final_picture,positions)
 
 
 
@@ -35,20 +47,28 @@ if __name__ == '__main__':
     #getting a picture as an input
     picture = input('Picture: ')
     picture = Image.open(picture)
-    chunk_size = input('Chunk Size: ')
+    print(picture.size)
+    num_chunks_x = int(input('Number Of Chunks Per Row: '))
+    num_chunks_y = int(input('Number Of Rows: '))
 
-    new_picture = get_chunks(picture,20,20)
+
+    new_picture = get_chunks(picture,num_chunks_x,num_chunks_y)
+    new_pos = new_picture[1]
+    new_picture = new_picture[0]
+
+
+    pic = ImageTk.PhotoImage(picture)
+    canvas.create_image(0,0,image=pic,anchor=NW)
 
     x_pos = 0
     y_pos = 0
     imgs = []
-    for y in new_picture:
-        for x in y:
-            img = ImageTk.PhotoImage(x)
-            canvas.create_image(y_pos,x_pos,image=img)
-            imgs.append(img)
-            x_pos += picture.height/19
-        x_pos = 0
-        y_pos += picture.width/19
+    for x in range(len(new_picture)):
+        img = ImageTk.PhotoImage(new_picture[x])
+        pos = new_pos[x]
+        canvas.create_image(pos[0],pos[1],image=img,anchor=NW)
+        imgs.append(img)
+        root.update()
+        time.sleep(.01)
 
     root.mainloop()
